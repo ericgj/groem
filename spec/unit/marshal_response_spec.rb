c572609f9e3fd580b695f53e7730b6374b5efda1
Eric Gjertsen <ericgj72@gmail.com>
2010-10-03 18:01:59 -0400

    spec'd Marshal::Response.load for OK and ERROR responses

--- spec/unit/marshal_response_spec.rb
+++ spec/unit/marshal_response_spec.rb
@@ -4,19 +4,95 @@ describe 'EM_GNTP::Marshal::Response.load' do
   
   
   describe 'when valid REGISTER response -OK' do
-  
+    # TODO
   end
   
   describe 'when valid REGISTER response -ERROR' do
-  
+    #TODO
   end
   
   describe 'when valid NOTIFY response -OK' do
   
+    before do
+      @input = <<-__________
+GNTP/1.0 -OK NONE
+Response-Action: NOTIFY
+Application-Name: SurfWriter 
+ Notification-ID : 999
+
+__________
+      dummy = Class.new { include(EM_GNTP::Marshal::Response) }
+      @subject = dummy.load(@input, false)
+    end
+
+    it 'should return three element array' do
+      @subject.must_be_kind_of Array
+      @subject.size.must_equal 3
+      puts
+      puts '--------- EM_GNTP::Marshal::Response.load when valid NOTIFY response -OK ----------'
+      puts @subject.inspect
+    end
+    
+    it 'should return OK status code (== 0)' do
+      @subject[0].to_i.must_equal 0
+    end
+    
+    it 'should return headers hash with 3 keys' do
+      @subject[1].keys.size.must_equal 3
+    end
+    
+    it 'should return Notification-ID header matching input' do
+      @subject[1]['notification_id'].must_equal '999'
+    end
+    
+    it 'should return nil result' do
+      @subject[2].must_equal nil
+    end
+        
   end
   
   describe 'when valid NOTIFY response -ERROR' do
   
+    before do
+      @input = <<-__________
+GNTP/1.0 -ERROR NONE
+ Error-Code: 303 
+Error-Description : REQUIRED_HEADER_MISSING 
+Response-Action: NOTIFY
+
+__________
+      dummy = Class.new { include(EM_GNTP::Marshal::Response) }
+      @subject = dummy.load(@input, false)
+    end
+
+    it 'should return three element array' do
+      @subject.must_be_kind_of Array
+      @subject.size.must_equal 3
+      puts
+      puts '--------- EM_GNTP::Marshal::Response.load when valid NOTIFY response -ERROR ----------'
+      puts @subject.inspect
+    end
+    
+    it 'should return status code matching input error code (== 303)' do
+      @subject[0].to_i.must_equal 303
+    end
+    
+    it 'should return headers hash with 2 keys' do
+      @subject[1].keys.size.must_equal 2
+    end
+    
+    it 'should return Response-Action header matching input' do
+      @subject[1]['response_action'].must_equal 'NOTIFY'
+    end
+    
+    it 'should not return header for Error-Code' do
+      @subject[1].keys.wont_include 'error_code'
+    end
+    
+    it 'should return nil result' do
+      @subject[2].must_equal nil
+    end
+    
   end
 
   describe 'when valid NOTIFY response -CALLBACK' do
